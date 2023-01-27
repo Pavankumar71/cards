d95ee9884833906a815e17433cfdd1304912b26b
Pavan <pn8189977@gmail.com>
2023-01-27 06:38:44 +0000

    cardsSloution

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+    // Write your code here.
+    <div className = bg-container>
+        <div>
+            <h1 class ="heading">Congratulations</h1>
+            <div class ="sub-container">
+                <div>
+                    <img src ="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" class ="image">
+                    <h1 class ="name">Kiran V</h1>
+                    <p class ="paragraph">Vishnu Institue of computer Education and Technology Bhimavaram </p>
+                    <img src ="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" class ='image1'>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+  
+ReactDOM.render(element, document.getElementById('root'))
